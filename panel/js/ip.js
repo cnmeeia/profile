7310c27c54a3d9f4c312d2524e9f11e7bab6190b
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-13 16:12:45 +0800

    更新 ip.js

--- panel/js/ip.js
+++ panel/js/ip.js
@@ -6,7 +6,7 @@ $httpClient.get(url, function(error, response, data){
     let ip = jsonData.query
   body = {
     title: "代理",
-    content: `${ip}\n${as}`,
+    content: `${ip}`,
   }
   $done(body);
 });
