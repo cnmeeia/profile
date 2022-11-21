44e60c55efaa3c2f10ed9a76d1a129c29e0b1693
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-21 20:42:18 +0800

    更新 ip.js

--- panel/js/ip.js
+++ panel/js/ip.js
@@ -1,12 +1,11 @@
 let url = "http://ip-api.com/json"
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
-    let country = jsonData.country
+    let countryCode = jsonData.countryCode
     let isp = jsonData.isp
     let ip = jsonData.query
   body = {
-    title: "Server",
-    content: `${country} ➟ ${ip}`,
+    content: `${countryCode} ➟ ${ip}`,
   }
   $done(body);
 });
