49cf93f77c90195d3cf3a291cce366354bd6287f
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-21 20:53:08 +0800

    更新 ip.js

--- panel/js/ip.js
+++ panel/js/ip.js
@@ -5,7 +5,7 @@ $httpClient.get(url, function(error, response, data){
     let isp = jsonData.isp
     let ip = jsonData.query
   body = {
-    title:"Server"
+    title: "Server",
     content: `${Code} ➟ ${ip}`,
   }
   $done(body);
