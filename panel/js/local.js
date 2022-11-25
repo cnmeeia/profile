d0e31a4014d9cfac9f1ebe4aa84893c15b4effba
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-26 07:51:58 +0800

    更新 local.js

--- panel/js/local.js
+++ panel/js/local.js
@@ -1,11 +1,11 @@
-let url = ""
+let url = "https://ipinfo.io/json"
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
     let Code = jsonData.country
     let ip = jsonData.ip
   body = {
     title: "本地",
-    content: `${Code} ➟ ${ip}`,
+    content: `${Code} ➟ ${ip}',
   }
   $done(body);
 });
