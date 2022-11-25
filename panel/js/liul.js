4e7fc89e75e667f43e4b04796920dba96936b4d8
meeia <102783643+cnmeeia@users.noreply.github.com>
2022-11-25 16:27:18 +0800

    更新 liul.js

--- panel/js/liul.js
+++ panel/js/liul.js
@@ -8,7 +8,7 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`${bytesToSize(used)}`];
+  let content = [`⬇️ ${bytesToSize(used)}`];
 
 
 
