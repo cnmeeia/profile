544cf2ed51fdc777a74c9483de8934ad47bf3c13
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-02-06 14:51:06 +0800

    更新 ip.js

--- panel/js/ip.js
+++ panel/js/ip.js
@@ -2,11 +2,12 @@ let url = "http://ip-api.com/json"
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
     let Code = jsonData.countryCode
+    let CT = jsonData.country
     let isp = jsonData.isp
     let ip = jsonData.query
   body = {
     title: "代理",
-    content: `${Code} ➟ ${ip}`,
+    content: `${CT} ➟ ${ip}`,
   }
   $done(body);
 });
