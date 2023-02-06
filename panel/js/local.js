20a79b6f5b9bd8979cfa9b83eac9614f85544cb0
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-02-06 15:01:26 +0800

    更新 local.js

--- panel/js/local.js
+++ panel/js/local.js
@@ -1,10 +1,10 @@
 let url = "https://ipinfo.io/json"
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
-    let Code = jsonData.country
+    let Code = jsonData.city
     let ip = jsonData.ip
   body = {
-    title: "本地",
+    title: "运营商ip",
     content: `${Code} ➟ ${ip}',
   }
   $done(body);
