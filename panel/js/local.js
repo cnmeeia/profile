dc65944778fa0a659542255ffe5a66e4fa65c077
meeia <102783643+cnmeeia@users.noreply.github.com>
2023-02-20 11:43:33 +0800

    更新 local.js

--- panel/js/local.js
+++ panel/js/local.js
@@ -1,11 +1,12 @@
+
 let url = "https://ipinfo.io/json"
 $httpClient.get(url, function(error, response, data){
     let jsonData = JSON.parse(data)
-    let Code = jsonData.city
     let ip = jsonData.ip
+    let city = jsonData.city
   body = {
     title: "运营商",
-    content: `${Code} ➟ ${ip}',
+    content: `${city} ➟ ${ip}`,
   }
   $done(body);
 });
